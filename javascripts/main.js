ec600fd211b6221f8766b445510bb6027c9f0f2b
Luis Felipe <xLuis190@hotmail.com>
2016-12-20 03:41:20 -0500

    first commit

---
+++ javascripts/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+  var h2Element = $("h2");
+  var button = $("#getJoke");
+  button.on('click',function(){
+    var name = $("#name").val(),
+    lastName = $("#lastname").val();
+    if(name === "" || lastName === ""){
+      alert("Put your name")
+      return;
+    }
+      h2Element.html(" ");
+      h2Element.removeClass("animated fadeIn")
+      $.ajax({
+      url:"http://api.icndb.com/jokes/random?firstName="+name+"&lastName="+lastName+"&limitTo=[nerdy]",
+      success: function(result){
+        h2Element.addClass("animated fadeIn text-xs-center");
+        h2Element.html(result.value.joke)
+
+      },
+      error: function(error){
+        console.log(error);
+      }
+    })
+  })
+
+});
